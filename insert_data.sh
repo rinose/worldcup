565f4afc24dd292249cc5a4187ccffff2d6c595e
Rino <rinosetola@gmail.com>
2024-04-04 23:34:14 +0200

    Update insert_data.sh

--- insert_data.sh
+++ insert_data.sh
@@ -26,17 +26,17 @@ csv_file="games.csv"
 
 # Read the CSV file line by line
 # game_id | year | round | winner_id | opponent_id | winner_goals | opponent_goals 
-# while IFS=',' read -r column1 column2 column3 column4 column5 column6
-# do
-#     if [ "$column3" != "winner" ]; then
-#       echo "Column3: $column3"
-#       echo "Column4: $column4"
-#       sql="insert into $teams_table (name) values ('$column3');"
-#       $PSQL "$sql"
-#       sql="insert into $teams_table (name) values ('$column4');"
-#       $PSQL "$sql"
-#     fi
-# done < "$csv_file"
+ while IFS=',' read -r column1 column2 column3 column4 column5 column6
+ do
+     if [ "$column3" != "winner" ]; then
+       echo "Column3: $column3"
+       echo "Column4: $column4"
+       sql="insert into $teams_table (name) values ('$column3');"
+       $PSQL "$sql"
+       sql="insert into $teams_table (name) values ('$column4');"
+       $PSQL "$sql"
+     fi
+ done < "$csv_file"
 
 # fill games 
 # game_id | year | round | winner_id | opponent_id | winner_goals | opponent_goals 
